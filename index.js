2a7476d37c9242e9cd2df216686e04b215f2f728
thebridge-tech <david.carvajal@thebridgeschool.es>
2020-10-26 11:57:25 +0100

    MathJS example

---
+++ index.js
@@ -0,0 +1,9 @@
+//const { sqrt } = require('mathjs');
+const math = require('mathjs');
+
+console.log("Hola mundo AHORA EN BACKEND!!");
+
+let numero1 = 169;
+
+console.log(`La raíz cuadrada de ${numero1}
+              es ${ math.sqrt(numero1).toString() }`);
